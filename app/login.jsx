f088da9c90990aeea963bf3d02a73aba907f5966
tugbabingol <tugbaabiingoll@gmail.com>
2024-09-29 02:31:59 +0300

    SocialApp

--- app/login.jsx
+++ app/login.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useRef, useState } from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
 import Icon from '../assets/icons'
 import { StatusBar } from 'react-native-web'
@@ -7,17 +7,66 @@ import BackButton from '../components/BackButton'
 import { useRouter } from 'expo-router'
 import { hp, wp } from '../helpers/common'
 import { theme } from '../constants/theme'
+import Input from '../components/Input'
+import Button from '../components/Button'
 
 const Login = () => {
   const router = useRouter();
+  const emailRef=useRef("");
+  const passwordRef=useRef("");
+  cons[loading, setLoading] = useState(false);
+
+  const onSubmit = async ()=>{
+    if (!emailRef.current || !passwordRef.current) {
+      Alert.alert('Login', "please fill all the fields!");
+      return;
+    }
+    //good to go
+  }
   return (
-    <ScreenWrapper>
+    <ScreenWrapper bg="white">
       <StatusBar style="dark"/>
       <View style={styles.container}>
         <BackButton router={router}/>
         {/* welcome */}
         <View>
+          <Text style={styles.welcomeText}>Hey,</Text>
+          <Text style={styles.welcomeText}>Welcome Back</Text>
+        </View>
 
+        {/*form */}
+        <View style={styles.form}>
+          <Text style={{fontSize:hp(1.5), color: theme.colors.text}}>
+            Please login to continue
+          </Text>
+          <Input
+            icon={<Icon name="mail" size={26} strokeWidth={1.6}/> }
+            placeholder='Enter your email'
+            onChangeText={value=> emailRef.current = value}
+          />
+          <Input
+            icon={<Icon name="lock" size={26} strokeWidth={1.6}/> }
+            placeholder='Enter your password'
+            secureTextEntry
+            onChangeText={value=> passwordRef.current = value}
+          />
+          <Text style={styles.forgotPassword}>
+            Forgot Password?
+          </Text>
+          {/*button*/}
+          <Button title={'Login'} loading={loading} onPress={onSubmit} />
+        </View>
+
+        {/* footer */}
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>
+            Don't have an account?
+          </Text>
+          <Pressable onPress={()=> router.push('signUp')}>
+            <Text style={[styles.footerText, {color: theme.colors.primaryDark, fontWeight:theme.fonts.semibold}]}>
+              Sign up
+            </Text>
+          </Pressable>
         </View>
       </View>
     </ScreenWrapper>
